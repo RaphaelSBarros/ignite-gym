0de3cb3572e4011a25ecdd1d867e7fdd53f0d1fa
sz-raphael-barros <raphael.barros@szsolucoes.com.br>
2024-11-12 12:09:41 -0300

    Componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,14 +1,22 @@
 import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: "https://github.com/RaphaelSBarros.png" }}
+        w="$16"
+        h="$16"
+        alt="imagem do usuário"
+      />
+
       <VStack>
         <Text color="$gray100" fontSize="$sm">
           Olá,
         </Text>
         <Heading color="$gray100" fontSize="$md">
-          Usuário
+          Raphael Alexsandro
         </Heading>
       </VStack>
     </HStack>
