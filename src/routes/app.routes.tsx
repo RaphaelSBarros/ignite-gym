c59d4d3a4d27740909d021f20cd864e049b54063
sz-raphael-barros <raphael.barros@szsolucoes.com.br>
2024-11-12 11:41:24 -0300

    Estilizando Menu Ativo e Inativos

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -27,8 +27,16 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 export function AppRoutes() {
   const { tokens } = gluestackUIConfig;
   const iconsize = tokens.space["6"];
+
   return (
-    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: tokens.colors.green500,
+        tabBarInactiveTintColor: tokens.colors.gray200,
+      }}
+    >
       <Screen
         name="home"
         component={Home}
