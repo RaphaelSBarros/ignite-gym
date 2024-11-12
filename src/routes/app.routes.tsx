f9360c5368ca591a32b1bf8dc0372f49629d8d70
sz-raphael-barros <raphael.barros@szsolucoes.com.br>
2024-11-12 11:48:02 -0300

    Estilizando a Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -12,6 +12,7 @@ import { History } from "@screens/History";
 import { Home } from "@screens/Home";
 import { Profile } from "@screens/Profile";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
+import { Platform } from "react-native";
 
 type AppRoutes = {
   home: undefined;
@@ -35,6 +36,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: tokens.colors.green500,
         tabBarInactiveTintColor: tokens.colors.gray200,
+        tabBarStyle: {
+          backgroundColor: tokens.colors.gray600,
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: tokens.space["10"],
+          paddingTop: tokens.space["6"],
+        },
       }}
     >
       <Screen
@@ -67,7 +75,11 @@ export function AppRoutes() {
         }}
       />
 
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{ tabBarButton: () => null }}
+      />
     </Navigator>
   );
 }
