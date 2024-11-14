396ad556936faee0be2523865dbf26e3b91ddaaf
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-14 16:31:49 -0300

    Criando um contexto

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { GluestackUIProvider, Text } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from "@components/Loading";
 import { Routes } from "@routes/index";
+import { AuthContext } from "@contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +19,16 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      <AuthContext.Provider
+        value={{
+          id: "1",
+          name: "Raphael",
+          email: "raphael@email.com",
+          avatar: "raphael.png",
+        }}
+      >
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </GluestackUIProvider>
   );
 }
