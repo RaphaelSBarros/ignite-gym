94e15e2a0147fbca1ad788f0943022992a5ed3f9
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-14 15:34:33 -0300

    Entendendo Interceptors

--- src/service/api.ts
+++ src/service/api.ts
@@ -3,3 +3,14 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://192.168.25.132:3333",
 });
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("INTERCEPTOR RESPONSE =>", response);
+    return response;
+  },
+  (error) => {
+    console.log("INTERCEPTOR RESPONSE ERROR =>", error);
+    return Promise.reject(error);
+  }
+);
