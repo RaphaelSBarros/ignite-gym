73fb9796e017692aa4e64dd124bc9f98bb7dcb7e
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-14 15:26:12 -0300

    Axios Exceptions

--- src/screens/Signup.tsx
+++ src/screens/Signup.tsx
@@ -17,6 +17,8 @@ import { Controller, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { api } from "../service/api";
+import axios from "axios";
+import { Alert } from "react-native";
 
 type FormDataProps = {
   name: string;
@@ -54,8 +56,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post("/users", { name, email, password });
-    console.log(response.data);
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (
