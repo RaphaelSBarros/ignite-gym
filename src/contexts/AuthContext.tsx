126a5c38357f813fc8201a1b362269dbf89f46f9
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-18 10:17:43 -0300

    Compartilhando estados no contexto

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { UserDTO } from "@dtos/UserDTO";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -14,18 +14,14 @@ export const AuthContext = createContext<AuthContextDataProps>(
 );
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: "1",
+    name: "Raphael",
+    email: "raphael@email.com",
+    avatar: "raphael.png",
+  });
+
   return (
-    <AuthContext.Provider
-      value={{
-        user: {
-          id: "1",
-          name: "Raphael",
-          email: "raphael@email.com",
-          avatar: "raphael.png",
-        },
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
   );
 }
