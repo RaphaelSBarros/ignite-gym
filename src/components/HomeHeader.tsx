0c83d9f0e5922bce18500ab3a97ea78876ff2631
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-18 12:01:37 -0300

    Exibindo a foto padrão do usuário

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,16 @@
 import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
 import { UserPhoto } from "./UserPhoto";
 import { LogOut } from "lucide-react-native";
+import { useAuth } from "@hooks/useAuth";
+import defaultUserPhotoImg from "@assets/userPhotoDefault.png";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto
-        source={{ uri: "https://github.com/RaphaelSBarros.png" }}
+        source={user.avatar ? { uri: user.avatar } : defaultUserPhotoImg}
         w="$16"
         h="$16"
         alt="imagem do usuário"
@@ -17,7 +21,7 @@ export function HomeHeader() {
           Olá,
         </Text>
         <Heading color="$gray100" fontSize="$md">
-          Raphael Alexsandro
+          {user.name}
         </Heading>
       </VStack>
 
