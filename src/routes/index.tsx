c7130d7ab27784fec96ad11365faec468e8915f9
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-18 11:27:39 -0300

    Redirecionar o usuário logado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { AuthRoutes } from "./auth.routes";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { Box } from "@gluestack-ui/themed";
 import { useAuth } from "@hooks/useAuth";
+import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   const { user } = useAuth();
@@ -13,7 +14,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="$gray700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   );
