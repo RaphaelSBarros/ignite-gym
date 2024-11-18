12a7d2d72a1effb63f1f328d2aedc23f85e2e266
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-18 10:35:08 -0300

    Recapitulando o formulário de SignIn

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,24 +1,42 @@
+import { Controller, useForm } from "react-hook-form";
+import { useNavigation } from "@react-navigation/native";
 import {
+  VStack,
+  Image,
+  Text,
   Center,
   Heading,
-  Image,
   ScrollView,
-  Text,
-  VStack,
 } from "@gluestack-ui/themed";
 
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+
+import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
-import Logo from "@assets/logo.svg";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+
+type FormData = {
+  email: string;
+  password: string;
+};
 
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
   function handleNewAccount() {
     navigation.navigate("signUp");
+  }
+
+  function handleSignIn({ email, password }: FormData) {
+    console.log(email, password);
   }
 
   return (
@@ -35,36 +53,64 @@ export function SignIn() {
           alt="Pessoas treinando"
           position="absolute"
         />
-
         <VStack flex={1} px="$10" pb="$16">
           <Center my="$24">
-            <Logo />
+            <LogoSvg />
 
             <Text color="$gray100" fontSize="$sm">
-              Treine sua mente e seu corpo
+              Treine sua mente e o seu corpo.
             </Text>
           </Center>
 
-          <Center gap="$2">
-            <Heading color="$gray100">Acesse sua conta</Heading>
+          <Center>
+            <Heading
+              color="$gray100"
+              fontSize="$xl"
+              mb="$6"
+              fontFamily="$heading"
+            >
+              Acesse sua conta
+            </Heading>
 
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
+            <Controller
+              control={control}
+              name="email"
+              rules={{ required: "Informe o e-mail" }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  errorMessage={errors.email?.message}
+                />
+              )}
             />
-            <Input placeholder="Senha" secureTextEntry />
 
-            <Button title="Acessar" />
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: "Informe a senha" }}
+              render={({ field: { onChange } }) => (
+                <Input
+                  placeholder="Senha"
+                  secureTextEntry
+                  onChangeText={onChange}
+                  errorMessage={errors.password?.message}
+                />
+              )}
+            />
+
+            <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
           </Center>
 
-          <Center flex={1} justifyContent="flex-end" marginTop="$4">
-            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+          <Center mt="$24">
+            <Text color="$gray100" fontSize="$sm" mb={3} fontFamily="$heading">
               Ainda não tem acesso?
             </Text>
 
             <Button
-              title="Criar conta"
+              title="Criar Conta"
               variant="outline"
               onPress={handleNewAccount}
             />
