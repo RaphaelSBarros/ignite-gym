8ccac067d49d3fb247fcc3c8629bd543233df2e1
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-11-19 15:35:42 -0300

    Colocando requisições na fila

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,9 +1,14 @@
 import { storageAuthTokenGet } from "@storage/storageAuthToken";
 import { AppError } from "@utils/AppError";
 
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+};
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -12,6 +17,9 @@ type APIInstanceProps = AxiosInstance & {
 export const api = axios.create({
   baseURL: "http://192.168.25.132:3333",
 }) as APIInstanceProps;
+
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
@@ -28,6 +36,26 @@ api.registerInterceptTokenManager = (signOut) => {
             signOut();
             return Promise.reject(requestError);
           }
+
+          const originalRequestConfig = requestError.config;
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  };
+                  resolve(api(originalRequestConfig));
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+
+          isRefreshing = true;
         }
 
         signOut();
